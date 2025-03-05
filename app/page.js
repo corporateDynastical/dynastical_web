7fc8a11f4053207ac3ccde874b24e54e17289ccf
corporateDynastical <140395510+corporateDynastical@users.noreply.github.com>
2025-03-05 13:57:25 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -11,153 +11,80 @@ import Testimonial from "@components/Testimonial";
 export default function Home() {
   return (
     <div>
+      {/* Page Styling */}
+      <div className="h-screen flex flex-col items-center justify-center text-center px-5 font-sans">
+        <h1 className="text-4xl sm:text-5xl font-bold mb-2">
+          Welcome To Dynastical Communication Website
+        </h1>
+        <h4 className="text-xl sm:text-2xl font-medium">
+          Website Under Construction.
+        </h4>
+      </div>
+
+      {/* Page Components */}
       <HeroFinal />
       <About />
       <AboutUs />
       <Slider
         data={[
-          {
-            src: "/corpLogos/epb.png",
-          },
-          {
-            src: "/corpLogos/promax.png",
-          },
-          {
-            src: "/corpLogos/tapas.png",
-          },
-          {
-            src: "/corpLogos/vedbrahma.png",
-          },
-          {
-            src: "/corpLogos/k_ulfi-01.png",
-          },
-          {
-            src: "/corpLogos/rf_01.png",
-          },
-          {
-            src: "/corpLogos/c_arnival-01-01.png",
-          },
-          {
-            src: "/corpLogos/D_elilah123.png",
-          },
-          {
-            src: "/corpLogos/r_unbhumi-01-01.png",
-          },
-          {
-            src: "/corpLogos/s_adguru-01-01.png",
-          },
-          {
-            src: "/corpLogos/t_ps-01.png",
-          },
-          {
-            src: "/corpLogos/m_sfm-01.png",
-          },
-          {
-            src: "/corpLogos/a_k realty-01.png",
-          },
-          {
-            src: "/corpLogos/k_etkar-01.png",
-          },
-          {
-            src: "/corpLogos/t_rispirit-01-01-01.png",
-          },
+          { src: "/corpLogos/epb.png" },
+          { src: "/corpLogos/promax.png" },
+          { src: "/corpLogos/tapas.png" },
+          { src: "/corpLogos/vedbrahma.png" },
+          { src: "/corpLogos/k_ulfi-01.png" },
+          { src: "/corpLogos/rf_01.png" },
+          { src: "/corpLogos/c_arnival-01-01.png" },
+          { src: "/corpLogos/D_elilah123.png" },
+          { src: "/corpLogos/r_unbhumi-01-01.png" },
+          { src: "/corpLogos/s_adguru-01-01.png" },
+          { src: "/corpLogos/t_ps-01.png" },
+          { src: "/corpLogos/m_sfm-01.png" },
+          { src: "/corpLogos/a_k realty-01.png" },
+          { src: "/corpLogos/k_etkar-01.png" },
+          { src: "/corpLogos/t_rispirit-01-01-01.png" },
         ]}
       />
       <ClientsSlides />
       <Testimonial />
       <Slider
-        title={"Our Corporate Clients"}
-        href={"/corporate-clients"}
+        title="Our Corporate Clients"
+        href="/corporate-clients"
         onPage={true}
         data={[
-          {
-            src: "/corpLogos/epb.png",
-          },
-          {
-            src: "/corpLogos/promax.png",
-          },
-          {
-            src: "/corpLogos/tapas.png",
-          },
-          {
-            src: "/corpLogos/vedbrahma.png",
-          },
-          {
-            src: "/corpLogos/k_ulfi-01.png",
-          },
-          {
-            src: "/corpLogos/rf_01.png",
-          },
-          {
-            src: "/corpLogos/c_arnival-01-01.png",
-          },
-          {
-            src: "/corpLogos/D_elilah123.png",
-          },
-          {
-            src: "/corpLogos/r_unbhumi-01-01.png",
-          },
-          {
-            src: "/corpLogos/s_adguru-01-01.png",
-          },
-          {
-            src: "/corpLogos/t_ps-01.png",
-          },
-          {
-            src: "/corpLogos/m_sfm-01.png",
-          },
-          {
-            src: "/corpLogos/a_k realty-01.png",
-          },
-          {
-            src: "/corpLogos/k_etkar-01.png",
-          },
-          {
-            src: "/corpLogos/t_rispirit-01-01-01.png",
-          },
+          { src: "/corpLogos/epb.png" },
+          { src: "/corpLogos/promax.png" },
+          { src: "/corpLogos/tapas.png" },
+          { src: "/corpLogos/vedbrahma.png" },
+          { src: "/corpLogos/k_ulfi-01.png" },
+          { src: "/corpLogos/rf_01.png" },
+          { src: "/corpLogos/c_arnival-01-01.png" },
+          { src: "/corpLogos/D_elilah123.png" },
+          { src: "/corpLogos/r_unbhumi-01-01.png" },
+          { src: "/corpLogos/s_adguru-01-01.png" },
+          { src: "/corpLogos/t_ps-01.png" },
+          { src: "/corpLogos/m_sfm-01.png" },
+          { src: "/corpLogos/a_k realty-01.png" },
+          { src: "/corpLogos/k_etkar-01.png" },
+          { src: "/corpLogos/t_rispirit-01-01-01.png" },
         ]}
       />
       <Slider
-        title={"Our Political Clients"}
-        href={"/political-clients"}
+        title="Our Political Clients"
+        href="/political-clients"
         onPage={true}
         data={[
-          {
-            src: "/assets/political/ravindra.png",
-          },
-          {
-            src: "/assets/political/bapu_mankar.png",
-          },
-          {
-            src: "/assets/political/ravindra_bhegade.png",
-          },
-          {
-            src: "/assets/political/lahu.png",
-          },
-          {
-            src: "/assets/political/umesh.png",
-          },
-          {
-            src: "/assets/political/haribhau.png",
-          },
-          {
-            src: "/assets/political/shelar.png",
-          },
-          {
-            src: "/assets/political/ganesh.png",
-          },
-          {
-            src: "/assets/political/aditya.png",
-          },
-          {
-            src: "/assets/political/rajashree.png",
-          },
-          {
-            src: "/assets/political/himali.png",
-          },
-          {
-            src: "/assets/political/priyanka.png",
-          },
+          { src: "/assets/political/ravindra.png" },
+          { src: "/assets/political/bapu_mankar.png" },
+          { src: "/assets/political/ravindra_bhegade.png" },
+          { src: "/assets/political/lahu.png" },
+          { src: "/assets/political/umesh.png" },
+          { src: "/assets/political/haribhau.png" },
+          { src: "/assets/political/shelar.png" },
+          { src: "/assets/political/ganesh.png" },
+          { src: "/assets/political/aditya.png" },
+          { src: "/assets/political/rajashree.png" },
+          { src: "/assets/political/himali.png" },
+          { src: "/assets/political/priyanka.png" },
         ]}
       />
       <CompanyAndVentures />
