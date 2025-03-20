eec37127d597877d2f261258ddcb7316c3a9786b
corporateDynastical <140395510+corporateDynastical@users.noreply.github.com>
2025-03-20 16:55:07 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div> 
+    <div style={{ maxWidth: "100%", overflow: "hidden" }}> 
       <Image 
         src="/assets/images/Rebrand_1800-900 cover.png" 
         alt="Dynastical Clients" 
         width={1800} 
-        height={900} // Required when not using `fill`
+        height={900} 
+        style={{ width: "100%", height: "auto" }} // Ensures responsiveness
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 1800px"
+        priority // Loads the image faster
       />
     </div>
   );
 }
-
