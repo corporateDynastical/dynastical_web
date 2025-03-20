8b303c70b69706f485e42128426e705ebe15e9b3
corporateDynastical <140395510+corporateDynastical@users.noreply.github.com>
2025-03-20 16:39:13 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,9 @@
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
-      {/* Page Styling */}
-      <div className="h-screen flex flex-col items-center justify-center text-center px-5 font-sans">
-        <h1 className="text-4xl sm:text-5xl font-bold mb-2">
-          Welcome To Dynastical Communication Website
-        </h1>
-        <h4 className="text-xl sm:text-2xl font-medium">
-          Website Under Construction.
-        </h4>
-      </div>
-    </div>
+   <div className={style.picture}>
+<Image src= "/assets/images/Rebrand_1800-900 cover.png" alt="dynastical clients" fill />
+
+</div>
   );
 }
